33dd97f39484c929c819a31e63492ebb89d2c132
Irtiza hassan <irtiza0987poiu@gmail.com>
2024-08-23 02:23:37 +0500

    killer

---
+++ python_learning/variables.py
@@ -0,0 +1,59 @@
+message:str="how it can possible"
+print(message)
+message01:str="i am dead now"
+print("he says:",message01)
+message02:str=("damn bro")
+print("you",message02)
+name:str="irtiza"
+print(name)
+print(f'"hi {name},would you like to eat some pizza?"')
+personName:str="irtiza hassan"
+print(personName)
+print(personName.lower())
+print(personName.upper())
+print(personName.title())
+person:str="uchiha sasuke"
+quote:str="This is the path i walk.Not you or anyone can change that"
+print(f'"{person} once said:{quote}"')
+famous_person:str="naruto uzumaki"
+message03:str="Its not about being the best.Its about being better than you were yesterday"
+print(f'"{famous_person} said that,{message03}"')
+person_name:str="  hi,Irtiza  "
+person_name01:str="  hi,\n\irtiza "
+person_name04:str="  hi,\t\irtiza  "
+print(person_name04)
+print(person_name.lstrip())
+print(person_name.rstrip())
+print(person_name.strip())
+x=2
+y=3
+z=4
+sum=x+y+z
+print(sum)
+a=3
+b=5
+print(f'"before swap,a={a} and b={b}"')
+a,b=b,a
+print(f'after swap,a={a}  and b={b}"')
+color:str="White"
+print(color)
+color_name:str="purple"
+print(color_name)
+pet_name:str="buddy"
+pet_name="max"
+print(pet_name)
+sunshine01:str="sunshine"
+print(sunshine01)
+score:int=100
+print(score)
+score=120
+print(score)
+city:str="lahore"
+print(city)
+text:str="python programming"
+print(text.title())
+mixed_case:str="pYtHoN pRoGrAmMiNg"
+print(mixed_case.lower())
+print(mixed_case.upper())
+temperature:int=25
+print(f'"current temperature is {temperature} degrees"')
