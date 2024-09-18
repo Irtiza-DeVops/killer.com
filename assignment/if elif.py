a604c8da74b6a3fef1980e645061d7f00e019a4d
Irtiza.DeVops <irtiza0987poiu@gmail.com>
2024-09-18 22:53:47 +0500

    hi

---
+++ assignment/if elif.py
@@ -0,0 +1,25 @@
+# Take input from the user
+age = int(input("Enter your age: "))
+
+if age > 99:
+    print("You are older than 99!")
+elif age > 90:
+    print("You are older than 90!")
+elif age > 80:
+    print("You are older than 80!")
+elif age > 70:
+    print("You are older than 70!")
+elif age > 60:
+    print("You are older than 60!")
+elif age > 50:
+    print("You are older than 50!")
+elif age > 40:
+    print("You are older than 40!")
+elif age > 30:
+    print("You are older than 30!")
+elif age > 20:
+    print("You are older than 20!")
+elif age > 10:
+    print("You are older than 10!")
+else:
+    print("You are 10 years old or younger!")
---
+++ function.py
@@ -0,0 +1,17 @@
+def fow():
+    print('Hello World')
+    print('This is a test program') 
+    print('This program is created by Irtiza')
+    print('Goodbye')
+    print('This is the end of the program')
+    print('This program is created by Irtiza')
+    print('Hello World')
+    print('This is a test program')
+fow()
+def hello2(name:str,age:int):
+    print(f'hello {name},my age is {age}')
+hello2('irtiza',15)
+def don(a:int,b:int)->int:
+    return a+b
+print(don(5,7))
+    
